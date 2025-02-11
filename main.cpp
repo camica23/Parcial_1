e6d398d0b76522b7766778b20ace365534575734
toti <laura.andrade-c@mail.escuelaing.edu.co>
2025-02-11 13:22:57 -0500

    Agregando nuevos archivos main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    int a,b,sum; 
+    std:: cout << "ingresa numeros: ";
+    std:: cin >> a >> b; 
+    suma = a + b; 
+    std:: cout << "la suma es:\n" << suma; 
+    return 0
+}
